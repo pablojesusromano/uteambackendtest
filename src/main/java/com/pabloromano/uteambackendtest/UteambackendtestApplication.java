3e1a52d5ef14c55f6f45ebfbbea4fe3b4aa04a0e
Pablo Romano <patoromano22@gmail.com>
2025-04-22 18:25:11 -0300

    First commit

---
+++ src/main/java/com/pabloromano/uteambackendtest/UteambackendtestApplication.java
@@ -0,0 +1,13 @@
+package com.pabloromano.uteambackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UteambackendtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UteambackendtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pabloromano/uteambackendtest/UteambackendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabloromano.uteambackendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UteambackendtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
